754a38bbd25bc0f757ff761d39797203b3805886
bramosl <bramosl@unemi.edu.ec>
2025-05-30 19:43:23 -0500

    deploy

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+PORT = int(os.environ.get('PORT', 8001))
 
 ROOT_URLCONF = 'config.urls'
 
